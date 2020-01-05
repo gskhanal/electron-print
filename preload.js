35167bc8b89a0cc8442ceef85e580839deda5530
ghanshyam <ghanshyam.khanal@live.com>
2020-01-05 19:40:24 +0545

    Changed renderer code for listening to print button clicked event

--- preload.js
+++ preload.js
@@ -1,5 +1,3 @@
-const ipc = require('electron').ipcRenderer;
-
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
@@ -13,13 +11,3 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 })
 
-const printButton = document.getElementById('print-button');
-
-printButton.addEventListener('click', function(event){
-  ipc.send('print-document');
-});
-
-ipc.on('print-success', function(event){
-  const message = 'Print Successful';
-  document.getElementById('message').innerHTML = message;
-})
--- renderer.js
+++ renderer.js
@@ -4,3 +4,15 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+const ipc = require('electron').ipcRenderer;
+
+const printButton = document.getElementById('print-button');
+
+printButton.addEventListener('click', function(event){
+  ipc.send('print-document');
+});
+
+ipc.on('print-success', function(event){
+  const message = 'Print Successful';
+  document.getElementById('message').innerHTML = message;
+})
